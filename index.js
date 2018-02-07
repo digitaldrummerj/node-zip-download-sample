a13cfb14c4d3fc826683661f28cd923c71d73ed2
Justin James <justin.james@intel.com>
2018-02-06 18:14:38 -0700

    add: error handling

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 'use strict';
 
-var AdmZip = require('adm-zip');
-var request = require('superagent');
-var url = require('url');
+const AdmZip = require('adm-zip');
+const request = require('superagent');
+const fs = require('fs');
+const url = require('url');
 
+const repoName = 'node-zip-download-sample';
+const href = 'https://github.com/digitaldrummerj/' + repoName + '/archive';
+const zipFile = 'master.zip';
+const source = `${href}/${zipFile}`;
+const extractEntryTo = `${repoName}-master/`;
 
-cosnt repoName = 'node-zip-download-sample';
-const url = `https://github.com/digitaldrummerj/${repoName}`;
-const zipFile = 'master.zip'
-let source = `${url}/${zipFile}`;
-var extractEntryTo = `${repoName}-master/`;
+const outputDir = './';
+const fileName = url
+  .parse(source)
+  .pathname.split('/')
+  .pop();
 
-var fileName = url.parse(source).pathname.split('/').pop();
-var outputDir = `./${repoName}-extracted`;
-request.get(source)
-    .pipe(fs.createWriteStream(fileName))
-    .on('finish', function () {
-        var zip = new AdmZip(fileName);
-        zip.extractEntryTo(extractEntryTo, outputDir, false, true);
-    });
+console.log('href', href);
+console.log('source', source);
+console.log('extractEntryTo', extractEntryTo);
+console.log('outputDir', outputDir);
+console.log('fileName', fileName);
 
+request
+  .get(source)
+  .on('error', function(error) {
+    console.log(error);
+  })
+  .pipe(fs.createWriteStream(zipFile))
+  .on('finish', function() {
+    console.log('finish');
+    var zip = new AdmZip(zipFile);
+    zip.extractEntryTo(extractEntryTo, outputDir, false, true);
+  });
