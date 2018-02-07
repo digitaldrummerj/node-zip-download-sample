d7a1c71a06ad6205830509c595d1b8d92a0b3a20
Justin James <justin.james@intel.com>
2018-02-06 18:23:14 -0700

    remove: url library

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 'use strict';
 
-const AdmZip = require('adm-zip');
+const admZip = require('adm-zip');
 const request = require('superagent');
 const fs = require('fs');
-const url = require('url');
 
 const repoName = 'node-zip-download-sample';
-const href = 'https://github.com/digitaldrummerj/' + repoName + '/archive';
+const href = `https://github.com/digitaldrummerj/${repoName}/archive`;
 const zipFile = 'master.zip';
 const source = `${href}/${zipFile}`;
 const extractEntryTo = `${repoName}-master/`;
 const outputDir = `./${repoName}-master/`;
-const fileName = url
-  .parse(source)
-  .pathname.split('/')
-  .pop();
-
 console.log('href', href);
 console.log('source', source);
 console.log('extractEntryTo', extractEntryTo);
 console.log('outputDir', outputDir);
-console.log('fileName', fileName);
 
 request
   .get(source)
@@ -30,7 +23,7 @@ request
   .pipe(fs.createWriteStream(zipFile))
   .on('finish', function() {
     console.log('finished dowloading');
-    var zip = new AdmZip(zipFile);
+    var zip = new admZip(zipFile);
     console.log('start unzip');
     zip.extractEntryTo(extractEntryTo, outputDir, false, true);
     console.log('finished unzip');
